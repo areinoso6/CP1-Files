9af7ccff0bb74d7a6aa17a7ba09cb695378268cd
Reinoso <areinoso@I46221P64128016.local>
2019-06-12 14:04:29 -0500

    Adding file 1

---
+++ DataTypes.java
@@ -0,0 +1,11 @@
+
+public class DataTypes{
+	public static void main(String[] args){
+		System.out.println("Here are some data types:"); //String
+		System.out.println("20 + 11"); //String
+		System.out.println(20 + 11); //Integers
+		System.out.println(17.4 - 3.2); //Decimal
+		System.out.println("20" + "4"); //Integers
+		System.out.println(20 + 4); //Integers
+	}
+}
