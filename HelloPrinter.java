23b0a9f4a90a4ccf7c68bd2e5a2f67e6a41a733a
Reinoso <areinoso@I46221P64128016.local>
2019-06-12 14:08:22 -0500

    Adding file 6

---
+++ HelloPrinter.java
@@ -0,0 +1,9 @@
+public class HelloPrinter
+{
+public static void main(String[] args)
+{
+System.out.println("Hello");//displays text
+System.out.print("World");//displays text
+System.out.print("!");
+}
+}
